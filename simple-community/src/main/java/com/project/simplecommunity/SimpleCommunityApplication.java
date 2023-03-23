e51bbec69509c68c4d5d9a30147f75619ba91375
bonojuno <gtx540@naver.com>
2023-03-23 11:53:49 +0900

    스프링 부트 프로젝트 생성
    프로젝트 생성

---
+++ simple-community/src/main/java/com/project/simplecommunity/SimpleCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.project.simplecommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCommunityApplication.class, args);
+	}
+
+}
---
+++ simple-community/src/test/java/com/project/simplecommunity/SimpleCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.simplecommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
