0885f8da5e7ba4e3c3239a77440e3f8b40c14bd5
sGOM <pooh6195@naver.com>
2023-04-01 17:27:25 +0900

    #2 - 코드 정리 및 crsf disable
    - 필요 없는 코드를 삭제
    - 현재 필요 없는 CRSF 설정 disable 처리

--- simple-community/src/main/java/com/project/simplecommunity/config/SecurityConfig.java
+++ simple-community/src/main/java/com/project/simplecommunity/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.project.simplecommunity.config;
 
+import com.project.simplecommunity.dto.UserPrincipal;
+import com.project.simplecommunity.service.UserAccountService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,24 +18,19 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(auth -> auth
-                        //.requestMatchers().permitAll()
-                        //.anyRequest().authenticated()
-                        .anyRequest().permitAll()   // 일단 전부 허용, 이후에 변경
-                )
-                .formLogin(withDefaults())  // url 은 /login 이고 username, password 필드가 존재
-                .logout(logout -> logout.logoutSuccessUrl("/"))
-                //.csrf(csrf -> csrf.ignoringAntMatchers("/**"))
+                .authorizeHttpRequests()
+                    .anyRequest().permitAll()
+                .and()
+                    .formLogin(withDefaults())
+                    .logout(logout -> logout.logoutSuccessUrl("/"))
+                    .csrf().disable()   // TODO : POST 는 무조건 검사, 공부해야함
                 .build();
     }
 
-//    @Bean
-//    public UserDetailsService userDetailsService(UserAccountService userAccountService) {
-//        return username -> userAccountService
-//                .searchUser(username)
-//                .map(UserPrincipal::from)
-//                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다 - username: " + username));
-//    }
+    @Bean
+    public UserDetailsService userDetailsService(UserAccountService userAccountService) {
+        return username -> UserPrincipal.from(userAccountService.getUserAccount(username));
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
