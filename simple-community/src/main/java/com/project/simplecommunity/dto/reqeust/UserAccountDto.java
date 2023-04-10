13cbd6f6e19ccc2d1b6b3e39b55a3b8712811ac9
bonojuno <gtx540@naver.com>
2023-04-10 15:21:26 +0900

    #20 - UserAccountDto 다시 추가
    포스트 컨트롤러에서 서비스 단으로 유저 정보를 넘겨줄 때
    사용하기 위해 재 추가, 추후 `UserPrincipal`로 대체

--- simple-community/src/main/java/com/project/simplecommunity/dto/reqeust/UserAccountDto.java
+++ simple-community/src/main/java/com/project/simplecommunity/dto/reqeust/UserAccountDto.java
@@ -1,5 +1,7 @@
 package com.project.simplecommunity.dto.reqeust;
 
+import com.project.simplecommunity.domain.UserAccount;
+
 public record UserAccountDto(
         String userId,
         String password,
@@ -9,4 +11,17 @@ public record UserAccountDto(
     static public UserAccountDto of(String userId, String password, String email) {
         return new UserAccountDto(userId, password, email);
     }
+
+    public static UserAccountDto from(UserAccount entity) {
+        return UserAccountDto.of(
+                entity.getUserId(),
+                entity.getPassword(),
+                entity.getEmail()
+        );
+    }
+
+    public UserAccount toEntity() {
+        return UserAccount.of(userId, password, email);
+    }
+
 }
