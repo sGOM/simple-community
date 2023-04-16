85f0bbf451dac8050e2323d42aaa62e611e4a7ca
bonojuno <gtx540@naver.com>
2023-04-16 14:37:23 +0900

    #20 - PostHashtag: delete 수정
    기존 방식은 삭제 한 건 당 하나의 쿼리가 수행되었는데
    @Query 를 사용하여 직접 쿼리문을 작성해
    In 방식으로 한번의 쿼리에 삭제되도록 수정

--- simple-community/src/main/java/com/project/simplecommunity/repository/PostHashtagRepository.java
+++ simple-community/src/main/java/com/project/simplecommunity/repository/PostHashtagRepository.java
@@ -2,7 +2,10 @@ package com.project.simplecommunity.repository;
 
 import com.project.simplecommunity.domain.PostHashtag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,6 +13,9 @@ public interface PostHashtagRepository extends
         JpaRepository<PostHashtag, Long>,
         QuerydslPredicateExecutor<PostHashtag>
 {
-    void deleteAllByPost_Id(Long postId);
+    @Modifying
+    @Query(value = "delete from PostHashtag ph where ph.post.id=:postId")
+    void deletePhByPostId(@Param("postId") Long postId);
+
     List<PostHashtag> findAllByPost_Id(Long postId);
 }
